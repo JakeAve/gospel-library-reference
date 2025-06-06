405ea11b85109eba5c14b7ceda0fe66a2ee24e11
JakeAve <jacobjavery@gmail.com>
2025-06-05 20:34:01 -0600

    Auto update version to 0.1.1

--- routes/index.tsx
+++ routes/index.tsx
@@ -26,7 +26,7 @@ export default function Home() {
                 {" "}
               </svg>
             </a>
-            <span id="version">v0.1.0</span>
+            <span id="version">v0.1.1</span>
             <a href="/" aria-label="Update" title="Update">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
